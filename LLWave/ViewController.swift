7288740c825bd25540304421e1070ac3e496161d
leefenghy <578545715@qq.com>
2017-09-06 14:29:46 +0800

    本地仓库添加到git

--- LLWave/ViewController.swift
+++ LLWave/ViewController.swift
@@ -13,6 +13,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let wave  = WaveView.init(frame: CGRect.init(x: 0, y: 0, width: self.view.bounds.width, height: self.view.bounds.height / 2))
+        wave.center = self.view.center
+        wave.color1 = UIColor.magenta
+        wave.color2 = UIColor.cyan
+        self.view.addSubview(wave)
+        wave.start()
     }
 
     override func didReceiveMemoryWarning() {
